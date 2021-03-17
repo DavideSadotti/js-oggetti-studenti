be2f5b7ee48af136903f26ac8e5da64da572f1bb
Davide <d.sadotti@gmail.com>
2021-03-17 20:15:49 +0100

    Step 2 Finito

--- js/script.js
+++ js/script.js
@@ -8,5 +8,33 @@ var studente = {
 var lista = $('#lista');
 
 for(var key in studente){
-  lista.append("<li>" + key.charAt(0).toUpperCase() + key.substr(1).toLowerCase() + ":" + " " + studente[key] + "</li>");
+  lista.append("<li>" + key.charAt(0).toUpperCase() + key.substr(1).toLowerCase() + ": " + studente[key] + "</li>");
+}
+
+
+// CREO UN ARRAY DI OGGETTI DI STUDENTI E POI LI STAMPO A VIDEO
+var studenti = [
+  {
+    nome: 'Aldo',
+    cognome: 'Baglio',
+    eta: 45
+  },
+  {
+    nome: 'Giovanni',
+    cognome: 'Storti',
+    eta: 56
+  },
+  {
+    nome: 'Giacomo',
+    cognome: 'Poretti',
+    eta: 64
+  }
+];
+
+var listaStudenti = $('#listaStudenti');
+
+for(var i = 0; i < studenti.length; i++){
+  for(var key in studenti[i]){
+    listaStudenti.append("<li>" + key.charAt(0).toUpperCase() + key.substr(1).toLowerCase() + ": " + studenti[i][key] + "</li>");
+  }
 }
